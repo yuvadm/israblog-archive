2e4bbc4038d9127d6dacb01f49a625988d8aa892
Yuval Adam <yuv.adm@gmail.com>
2014-03-29 02:00:23 +0300

    Initial scrapy project

---
+++ israblog/israblog/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for israblog project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'israblog'
+
+SPIDER_MODULES = ['israblog.spiders']
+NEWSPIDER_MODULE = 'israblog.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'israblog (+http://www.yourdomain.com)'
